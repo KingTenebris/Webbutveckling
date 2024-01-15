b4623f92718546b910ee0974cfb91eb88d5c23c9
LordTenebris <113670625+KingTenebris@users.noreply.github.com>
2024-01-15 14:06:01 +0100

    Light on and Light off

--- javascript/Lektion 2 Events/script.js
+++ javascript/Lektion 2 Events/script.js
@@ -1,11 +1,18 @@
+let white = "rgb(255, 255, 255)";
+let black = "rgb(0, 0, 0)";
+let orange = "rgb(255, 165, 0)";
+
+let svgColor = document.getElementsByClassName("svgColor");
+
+
 let newH1 = document.getElementsByTagName("h1");
 //* knapp för dark and light mode
 let mode = false;
 //! javaScript behöver RBG code i ""
-newH1[0].style.color = "rgb(0, 0, 0)";
+newH1[0].style.color = black;
 
 newH1[0].addEventListener("mouseover", function(){
-    newH1[0].style.color = "rgb(255, 140, 0)";
+    newH1[0].style.color = orange;
 
     //* if-sats in JavaScript 
     // if(newH1[0].style.color == "rgb(0, 0, 0)"){
@@ -18,10 +25,10 @@ newH1[0].addEventListener("mouseover", function(){
 
 newH1[0].addEventListener("mouseleave", function(){
     if(mode == false){
-        newH1[0].style.color = "rgb(0, 0, 0)";
+        newH1[0].style.color = black;
     }
     else{
-        newH1[0].style.color = "rgb(255, 255, 255)";
+        newH1[0].style.color = white;
     }
 });
 
@@ -31,34 +38,41 @@ let body = document.querySelector("body");
 let textMode = document.getElementsByClassName("textMode");
 modeButton[0].addEventListener("click", function(){
     if(mode == false){
-        body.style.backgroundColor = "rgb(0, 0, 0)";
+        body.style.backgroundColor = black;
+        lightDiv[0].style.backgroundColor = body.style.backgroundColor;
+        svgColor[0].style.color = white;
         newH1[0].innerHTML = "And now the fun begins";
-        newH1[0].style.color = "rgb(255, 255, 255)";
+        newH1[0].style.color = white;
         textMode[0].innerHTML = "Light Mode!"
         mode = true;
     }
     else{
-        body.style.backgroundColor = "rgb(255, 255, 255)";
+        body.style.backgroundColor = white;
+        lightDiv[0].style.backgroundColor = body.style.backgroundColor;
+        svgColor[0].style.color = black;
         newH1[0].innerHTML = "Welcome to my Webbsite with some JS";
-        newH1[0].style.color = "rgb(0, 0, 0)";
-        textMode[0].innerHTML = "Dark Mode!"
+        newH1[0].style.color = black;
+        textMode[0].innerHTML = "Dark Mode!";
         mode = false;
     }
 });
 
 //* Light's on or off
 let light = document.getElementsByTagName("img");
+let lightDiv = document.getElementsByClassName("lightDiv");
 // button for light is second in list so [1] - modeButton[1]
 let lightMode = false;
 modeButton[1].addEventListener("click", function(){
     if(lightMode == false){
-        light[0].src = "emoji_objects_FILL1_wght400_GRAD0_opsz24.svg";
-        modeButton[1].innerHTML = "Light off"
+        light[0].src = "emoji_objects_FILL0_wght400_GRAD0_opsz24.svg";
+        lightDiv[0].style.backgroundColor = orange;
+        modeButton[1].innerHTML = "Light off";
         lightMode = true;
     }
     else{
-        light[0].src = "emoji_objects_FILL0_wght400_GRAD0_opsz24.svg";
-        modeButton[1].innerHTML = "Light on"
+        light[0].src = "emoji_objects_FILL1_wght400_GRAD0_opsz24.svg";
+        lightDiv[0].style.backgroundColor = body.style.backgroundColor;
+        modeButton[1].innerHTML = "Light on";
         lightMode = false;
     }
 
